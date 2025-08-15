e7330b8b84721a7ae3654998c5db7f689f046b51
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2025-08-15 12:03:50 +0530

    Learn about catch all segement and ssr csr ssr

---
+++ src/app/blog2/[...folderId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Catch all route
+        
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/blog2/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>B;log 2 page is here </div>
+  )
+}
+
+export default page 
